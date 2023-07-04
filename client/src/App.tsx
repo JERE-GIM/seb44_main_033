4b652b9c7701a2dbd6ae2b75872a4255f33f99af
hahagarden <tingyuan413@gmail.com>
2023-07-03 19:33:35 -0700

    feat: 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './pages/Root';
+import Main from './pages/Main';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
+import Watchlist from './pages/Watchlist';
+import Movie from './pages/Movie';
+import Search from './pages/Search';
+import Statistics from './pages/Statistics';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '', element: <Main /> },
+      { path: 'signup', element: <Signup /> },
+      { path: 'login', element: <Login /> },
+      { path: 'mypage', element: <Mypage /> },
+      { path: 'watchlist', element: <Watchlist /> },
+      { path: 'movie/:id', element: <Movie /> },
+      { path: 'search', element: <Search /> },
+      { path: 'statistics', element: <Statistics /> },
+    ],
+  },
+]);
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/Root.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/share/Header';
+import Footer from '../components/share/Footer';
+import { StyledRoot } from './styles/Root.styled';
+
+export default function Root() {
+  return (
+    <StyledRoot>
+      <Header />
+      <Outlet />
+      <Footer />
+    </StyledRoot>
+  );
+}
