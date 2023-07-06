c5da77de7bf5242a3e17edab6212e1528da8ec7e
hahagarden <tingyuan413@gmail.com>
2023-07-06 02:07:51 -0700

    feat: 영화 정보 컴포넌트 UI 구현

--- client/src/pages/Movie.tsx
+++ client/src/pages/Movie.tsx
@@ -5,7 +5,14 @@ import { dummyReviews } from '../dummy/dummyReview';
 import Rating from '../components/share/Rating';
 import {
   StyledMovieCover,
+  StyledMovieDetail,
+  StyledMovieDetailCol,
   StyledMovieInfo,
+  StyledMovieInfoCol,
+  StyledMovieInfoContainer,
+  StyledMovieInfoSpan,
+  StyledMovieInfoText,
+  StyledMoviePoster,
   StyledMyReviewButtons,
   StyledMyReviewContainer,
   StyledMyReviewContent,
@@ -15,6 +22,7 @@ import {
   StyledStarsContainer,
 } from './styles/Movie.styled';
 import ConfirmModal from '../components/movie/ConfirmModal';
+import elementalPoster from '../assets/elemental_poster.png';
 
 export default function Movie() {
   // 더미데이터 사용
@@ -51,30 +59,89 @@ export default function Movie() {
           <Rating rating={rating} setRating={setRating} />
         </StyledStarsContainer>
       </StyledMovieCover>
-      <StyledMovieInfo>
-        {review ? (
-          <StyledMyReviewContainer>
-            <StyledMyReviewContent>{review.comment}</StyledMyReviewContent>
-            <StyledMyReviewButtons>
-              <StyledMyReviewControlButton onClick={handleOpenReviewModal}>
-                수정
-              </StyledMyReviewControlButton>
-              <StyledMyReviewControlButton onClick={handleOpenConfirmModal}>
-                삭제
-              </StyledMyReviewControlButton>
-            </StyledMyReviewButtons>
-          </StyledMyReviewContainer>
-        ) : (
-          <StyledMyReviewContainer>
-            <StyledMyReviewContent>
-              영화를 보고 난 소감을 기록하세요.
-            </StyledMyReviewContent>
-            <StyledMyReviewRegisterButton onClick={handleOpenReviewModal}>
-              리뷰 작성하기
-            </StyledMyReviewRegisterButton>
-          </StyledMyReviewContainer>
-        )}
-      </StyledMovieInfo>
+      <StyledMovieDetail>
+        <StyledMovieDetailCol>
+          <StyledMoviePoster src={elementalPoster} alt="poster image" />
+        </StyledMovieDetailCol>
+        <StyledMovieDetailCol>
+          {review ? (
+            <StyledMyReviewContainer>
+              <StyledMyReviewContent>{review.comment}</StyledMyReviewContent>
+              <StyledMyReviewButtons>
+                <StyledMyReviewControlButton onClick={handleOpenReviewModal}>
+                  수정
+                </StyledMyReviewControlButton>
+                <StyledMyReviewControlButton onClick={handleOpenConfirmModal}>
+                  삭제
+                </StyledMyReviewControlButton>
+              </StyledMyReviewButtons>
+            </StyledMyReviewContainer>
+          ) : (
+            <StyledMyReviewContainer>
+              <StyledMyReviewContent>
+                영화를 보고 난 소감을 기록하세요.
+              </StyledMyReviewContent>
+              <StyledMyReviewRegisterButton onClick={handleOpenReviewModal}>
+                리뷰 작성하기
+              </StyledMyReviewRegisterButton>
+            </StyledMyReviewContainer>
+          )}
+          <StyledMovieInfo>
+            <StyledMovieInfoContainer>
+              <StyledMovieInfoCol>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>개봉</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>{movie.openAt}</StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>장르</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>
+                    {movie.genres.join('/')}
+                  </StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>국가</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>{movie.nation}</StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>등급</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>{movie.age}</StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>러닝타임</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>
+                    {movie.runningTime}분
+                  </StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>출연</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>
+                    {movie.actors.join(', ')}
+                  </StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+              </StyledMovieInfoCol>
+              <StyledMovieInfoCol>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>평점</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>
+                    {movie.averageRating}
+                  </StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>누적관객</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>
+                    {movie.audience.toLocaleString()}명
+                  </StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+                <StyledMovieInfoText>
+                  <StyledMovieInfoSpan>박스오피스</StyledMovieInfoSpan>
+                  <StyledMovieInfoSpan>{movie.boxOffice}위</StyledMovieInfoSpan>
+                </StyledMovieInfoText>
+              </StyledMovieInfoCol>
+            </StyledMovieInfoContainer>
+          </StyledMovieInfo>
+        </StyledMovieDetailCol>
+      </StyledMovieDetail>
       <StyledReviewList></StyledReviewList>
       {isOpenReviewModal && (
         <ReviewRegisterModal
