744469c2f259786375ed8d06d26659e26e7c69fa
jonghyeon <jonghyeon37@naver.com>
2023-07-07 09:50:39 +0900

    feat: userid 수정

--- client/src/components/share/Header.tsx
+++ client/src/components/share/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Searchbar from '../share/Searchbar';
-import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../redux/store';
 import { RootState } from '../../redux/store';
 import { login, logout } from '../../redux/reducers/isLogin';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,16 +15,21 @@ import {
   MypageContainer,
   MyPageId,
 } from '../styles/Header.styled';
+interface Header {
+  username: string;
+}
 
-const Header: React.FC = () => {
-  const isLoggedIn = useSelector((state: RootState) => state.isLogin);
-  const dispatch = useDispatch();
+export default function Header() {
+  const isLoggedIn = useAppSelector((state: RootState) => state.isLogin);
+  const dispatch = useAppDispatch();
+  const [username, setusername] = useState('');
 
   const handleLogin = () => {
     dispatch(login());
   };
 
   const handleLogout = () => {
+    setusername('');
     dispatch(logout());
   };
   return (
@@ -40,7 +46,7 @@ const Header: React.FC = () => {
         <ButtonContainer>
           <MypageContainer>
             <FontAwesomeIcon icon={faCircleUser} size="2x" />
-            <MyPageId>닉네임몇글자까지할지모르겠네</MyPageId>
+            <MyPageId>{username}</MyPageId>
           </MypageContainer>
           <Button onClick={handleLogout}>로그아웃</Button>
           <Button>통계자료</Button>
@@ -54,6 +60,4 @@ const Header: React.FC = () => {
       )}
     </HeaderContainer>
   );
-};
-
-export default Header;
+}
