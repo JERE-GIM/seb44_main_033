0b8f3a7b97a831894a0eef63f02d1ba3145b5976
jonghyeon <jonghyeon37@naver.com>
2023-07-07 14:00:58 +0900

    design: Root.styled.ts 수정

--- client/src/pages/styles/Root.styled.ts
+++ client/src/pages/styles/Root.styled.ts
@@ -1,6 +1,9 @@
 import { styled } from 'styled-components';
 
 export const StyledRoot = styled.div`
-  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
   height: 100%;
 `;
