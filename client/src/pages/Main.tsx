f80bc90f6d2e0ce55e8dab38e66a63bcfd037c80
hyeong-il <coding0316@naver.com>
2023-07-07 16:24:57 +0900

    feat: 박스오피스 순위 버튼 캐러셀 슬라이더 UI 구현

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,3 +1,25 @@
-export default function Main() {
-  return <>Main</>;
-}
+import styled from 'styled-components';
+import Topmovie from '../components/main/Topmovie';
+
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  margin-top: 62px;
+  padding: 24px 0;
+`;
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <Main>
+        <Container>
+          <Topmovie />
+        </Container>
+      </Main>
+    </div>
+  );
+};
+export default MainPage;
