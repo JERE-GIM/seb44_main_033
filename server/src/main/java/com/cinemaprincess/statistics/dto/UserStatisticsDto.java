d0222f1c29173e5c6c9d8b5abdaddd96d76b076e
[haru lee] <hitenshi1997@gmail.com>
2023-07-13 06:52:34 +0900

    Feat: 구현 사이트이용자 성별/연령별 선호장르 통계

---
+++ server/src/main/java/com/cinemaprincess/statistics/dto/UserStatisticsDto.java
@@ -0,0 +1,16 @@
+package com.cinemaprincess.statistics.dto;
+
+import com.cinemaprincess.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@AllArgsConstructor
+@Setter
+@Getter
+public class UserStatisticsDto {
+    private List<String> genre;
+    private User.Gender gender;
+
+}
