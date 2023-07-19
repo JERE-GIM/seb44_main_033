3a18ce139f6beb3595ef1081dd731223f688707d
jonghyeon <jonghyeon37@naver.com>
2023-07-20 06:14:40 +0900

    fix: App.tsx 라우트 추가

--- client/src/components/login/Kakaologin.tsx
+++ client/src/components/login/Kakaologin.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { KakaoLogo } from '../styles/SignupForm1.styled';
 
-// 카카오 로그인 요청 URL을 만듭니다.
 const KAKAO_OAUTH_URL = `http://ec2-54-180-99-202.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/kakao`;
 
 const KakaoLogin: React.FC = () => {
-  // KakaoLogo 컴포넌트를 클릭하면, 카카오 로그인 요청 URL로 이동하도록 합니다.
   const handleLogin = () => {
     window.location.href = KAKAO_OAUTH_URL;
   };
