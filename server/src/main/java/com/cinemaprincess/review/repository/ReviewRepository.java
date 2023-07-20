b5af76628eb8f6e69649b6e7c95c6118ccb191bf
SuHyeonEo <tngus7520@gmail.com>
2023-07-20 17:43:19 +0900

    deploy

--- server/src/main/java/com/cinemaprincess/review/repository/ReviewRepository.java
+++ server/src/main/java/com/cinemaprincess/review/repository/ReviewRepository.java
@@ -2,11 +2,15 @@ package com.cinemaprincess.review.repository;
 
 import com.cinemaprincess.movie.entity.MovieDetail;
 import com.cinemaprincess.review.entity.Review;
+import com.cinemaprincess.review.projection.TopReviewedMoviesResponse;
 import com.cinemaprincess.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
