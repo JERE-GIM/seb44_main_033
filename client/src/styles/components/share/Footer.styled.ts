7a5f1b5a1ea5c9eef20f956db9afd5091abc8994
hahagarden <tingyuan413@gmail.com>
2023-07-26 01:04:36 +0900

    design: 푸터 css 수정

--- client/src/styles/components/share/Footer.styled.ts
+++ client/src/styles/components/share/Footer.styled.ts
@@ -11,18 +11,19 @@ export const FooterContainer = styled.footer`
   bottom: 0;
   left: 0;
   right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 
 export const BETeamContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 10px;
   font-size: 16px;
 `;
 export const FETeamContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 20px;
   font-size: 16px;
 `;
 export const TeamName = styled.div`
