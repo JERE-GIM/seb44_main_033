0bb33bc43b126b30fad27d6c3d95847fef26820f
hahagarden <tingyuan413@gmail.com>
2023-07-26 00:20:27 +0900

    rename: 스타일링 파일 이동

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './styles/GlobalStyle';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 const root = ReactDOM.createRoot(
